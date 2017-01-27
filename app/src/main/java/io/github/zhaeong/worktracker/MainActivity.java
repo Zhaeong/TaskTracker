188d73870e508a429b04f6695de7aeef1a805720
Zhaeong <owen_zzz@hotmail.com>
2017-01-27 01:09:03 -0500

    added navigation drawer to the right site

--- app/src/main/java/io/github/zhaeong/worktracker/MainActivity.java
+++ app/src/main/java/io/github/zhaeong/worktracker/MainActivity.java
@@ -19,23 +19,48 @@ import static io.github.zhaeong.worktracker.AddTaskMenu.RESULT_EDIT;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
-    private ArrayList<TaskObject> TaskList = new ArrayList<TaskObject>();
     public final static String TASK_NAME = "com.example.mainactivity.TASKNAME";
     public final static String TASK_DESCRIPTION = "com.example.mainactivity.TASKDESC";
     public final static String TASK_ID = "com.example.mainactivity.TASKID";
     public static CustomDBHelper myTaskDatabase;
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> drawerItemsArray = new ArrayList<String>();
+
+    public final static String AddNew = "Add New Task";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initiateDatabase();
+        addDrawerItems();
         populateList();
     }
 
     protected void initiateDatabase()
     {
         myTaskDatabase = new CustomDBHelper(this);
+    }
+
+    protected void addDrawerItems()
+    {
+        drawerItemsArray.add(AddNew);
+        mDrawerList = (ListView)findViewById(R.id.right_drawer);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drawerItemsArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(drawerItemsArray.get(position).equals(AddNew))
+                {
+                    addNewtask(view);
+                }
+                Toast.makeText(MainActivity.this, drawerItemsArray.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     protected void populateList()
@@ -51,11 +76,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-
-                //TaskObject clickedTaskObj = (TaskObject) parent.getItemAtPosition(position);
                 Intent intent = new Intent(MainActivity.this, AddTaskMenu.class);
-                intent.putExtra(TASK_NAME, "dsfdsfdsfsdfdsfds");
-                //intent.putExtra(TASK_DESCRIPTION, clickedTaskObj.Description);
+
                 intent.putExtra(TASK_ID, id);
 
 
@@ -81,9 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 String taskName = data.getStringExtra(AddTaskMenu.TASK_NAME_MENU);
                 String taskDesc = data.getStringExtra(AddTaskMenu.TASK_DESCRIPTION_MENU);
 
-                //TaskObject taskObj = new TaskObject(taskName, taskDesc);
                 myTaskDatabase.addTask(taskName, taskDesc);
-                //TaskList.add(taskObj);
                 populateList();
             }
             if(resultCode == RESULT_EDIT)
@@ -97,10 +117,7 @@ public class MainActivity extends AppCompatActivity {
 
                     myTaskDatabase.updateTask(taskPosit, taskName, taskDesc);
                     populateList();
-
                 }
-
-
             }
             if(resultCode == RESULT_DELETE)
             {
